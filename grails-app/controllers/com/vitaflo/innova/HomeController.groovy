64ee4eef3fbc731eb8413f2ad7ef93749bae1999
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2009-08-24 23:36:10 +0000

    I keep the prints if not the application won't work. Weird, isn't it?

--- grails-app/controllers/com/vitaflo/innova/HomeController.groovy
+++ grails-app/controllers/com/vitaflo/innova/HomeController.groovy
@@ -4,8 +4,10 @@ class HomeController {
 
     def index = { 
         if(!session?.countries){
+            print'\n @@@@@@@@@@@@@@@@@@@@@@otra vez sop@@@@@@@@@@@@@@@@@@@@@@@@@@\n'
             def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
             session.countries = user.countries
         }
+        print session
     }
 }
