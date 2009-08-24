bac80bec52b086830f7ef80d20b561b02a7a5bf7
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2009-08-24 23:22:49 +0000

    Deleted print

--- grails-app/controllers/com/vitaflo/innova/HomeController.groovy
+++ grails-app/controllers/com/vitaflo/innova/HomeController.groovy
@@ -4,10 +4,8 @@ class HomeController {
 
     def index = { 
         if(!session?.countries){
-            print'\n @@@@@@@@@@@@@@@@@@@@@@otra vez sopa@@@@@@@@@@@@@@\n'
             def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
             session.countries = user.countries
         }
-        print session
     }
 }
