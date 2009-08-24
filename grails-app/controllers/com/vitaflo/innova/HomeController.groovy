72749989f10178617bff9c8456e4114d278abe0e
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2009-08-24 15:07:24 +0000

    Save the countries associated to the user in the httpSession

--- grails-app/controllers/com/vitaflo/innova/HomeController.groovy
+++ grails-app/controllers/com/vitaflo/innova/HomeController.groovy
@@ -2,5 +2,12 @@ package com.vitaflo.innova
 
 class HomeController {
 
-    def index = { }
+    def index = { 
+        if(!session?.countries){
+            print'\n @@@@@@@@@@@@@@@@@@@@@@otra vez sopa@@@@@@@@@@@@@@\n'
+            def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
+            session.countries = user.countries
+        }
+        print session
+    }
 }
