ff52bbf076111e843cd6110304738bc9b995345b
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2009-08-28 12:02:57 +0000

    Sort country by name

--- grails-app/controllers/com/vitaflo/innova/HomeController.groovy
+++ grails-app/controllers/com/vitaflo/innova/HomeController.groovy
@@ -6,7 +6,7 @@ class HomeController {
         if(!session?.countries){
             print'\n @@@@@@@@@@@@@@@@@@@@@@otra vez sop@@@@@@@@@@@@@@@@@@@@@@@@@@\n'
             def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
-            session.countries = user.countries
+            session.countries = user.countries.sort{it.name}
         }
         print session
     }
