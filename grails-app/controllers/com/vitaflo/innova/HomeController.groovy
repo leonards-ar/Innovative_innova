ca4174cddd2270f1e67d89ccd1a565aebc96f43e
Federico Barrera Oro <federico.barreraoro@mulesoft.com>
2009-08-29 17:48:26 +0000

    Delete unnecesary prints.

--- grails-app/controllers/com/vitaflo/innova/HomeController.groovy
+++ grails-app/controllers/com/vitaflo/innova/HomeController.groovy
@@ -2,12 +2,12 @@ package com.vitaflo.innova
 
 class HomeController {
 
-    def index = { 
-        if(!session?.countries){
-            print'\n @@@@@@@@@@@@@@@@@@@@@@otra vez sop@@@@@@@@@@@@@@@@@@@@@@@@@@\n'
-            def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
-            session.countries = user.countries.sort{it.name}
+    def index = {
+        User.withTransaction{
+            if(!session?.countries){
+                def user = User.findByUsername(session.SPRING_SECURITY_LAST_USERNAME)
+                session.countries = user.countries.sort{it.name}
+            }
         }
-        print session
     }
 }
