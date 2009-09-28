84a6ca81df96f343067d9020066ff80a4be125af
Mariano Capurro <marianocapurro@users.noreply.github.com>
2009-09-28 01:06:12 +0000

    Sorry folks! But wanted to commit mysql connection charset parameters!

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -8,26 +8,27 @@ hibernate {
     cache.use_second_level_cache=true
     cache.use_query_cache=true
     cache.provider_class='com.opensymphony.oscache.hibernate.OSCacheProvider'
+    show_sql=true
 }
 // environment specific settings
 environments {
 	development {
 		dataSource {
-			//dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+                    // dbCreate = "create-drop" // one of 'create', 'create-drop','update'
                     dbCreate = 'update'
-                    url = "jdbc:mysql://192.168.1.5/vitaflo_sistema"
+                    url = "jdbc:mysql://spmdb/vitaflo_sistema?useUnicode=true&characterEncoding=utf-8"
 		}
 	}
 	test {
 		dataSource {
 			dbCreate = "update"
-                        url = "jdbc:mysql://localhost/vitaflo_sistema"
+                        url = "jdbc:mysql://localhost/vitaflo_sistema?useUnicode=true&characterEncoding=utf-8"
 		}
 	}
 	production {
 		dataSource {
 			dbCreate = "update"
-                        url = "jdbc:mysql://localhost/vitaflo_sistema"
+                        url = "jdbc:mysql://localhost/vitaflo_sistema?useUnicode=true&characterEncoding=utf-8"
 		}
 	}
 }
